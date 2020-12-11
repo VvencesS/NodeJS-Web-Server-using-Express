69d2c1fe037b9dd217c8543e1ceab64d3e2c8955
VvencesS <DucSoanVences99@gmail.com>
2020-12-11 15:07:38 +0700

    Tim hieu ve phuong thuc POST va req.body đe lay du lieu tu form submit len

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
 app.set('view engine', 'pug');
 app.set('views', './views');
 
@@ -31,6 +34,15 @@ app.get('/users/search', (req, res) => {
     res.render('users/index', {
         users: matchUsers,
     });
+});
+
+app.get('/users/create', (req, res) => {
+    res.render('users/create');
+});
+
+app.post('/users/create', (req, res) => {
+    users.push(req.body);
+    res.redirect('/users');
 });
 
 app.listen(port, () => {
