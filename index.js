7a3c92c696347f7bfdcd07ea1ebfb3f7caa8bfef
VvencesS <DucSoanVences99@gmail.com>
2020-12-11 14:32:19 +0700

    Su dung template engines Pug vơi Express

--- index.js
+++ index.js
@@ -2,8 +2,22 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'pug');
+app.set('views', './views');
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.render('index', {
+        name: 'Soạn',
+    });
+});
+
+app.get('/users', (req, res) => {
+    res.render('users/index', {
+        users: [
+            { id: 1, name: 'Jane' },
+            { id: 2, name: 'John' },
+        ]
+    });
 });
 
 app.listen(port, () => {
