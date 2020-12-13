68d7be4c889945bab786ab483d5de933a3d10614
VvencesS <DucSoanVences99@gmail.com>
2020-12-13 10:13:36 +0700

    Tich hop md5 de ma hoa mat khau

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,3 +1,5 @@
+const md5 = require('md5');
+
 const db = require('../routers/db');
 
 module.exports.login = (req, res) => {
@@ -19,7 +21,9 @@ module.exports.postLogin = (req, res) => {
         });
         return;
     }
-    if(user.password !== password) {
+
+    let hashedPassword = md5(password);
+    if(user.password !== hashedPassword) {
         res.render('auth/login', {
             errors: [
                 'Wrong password!'
