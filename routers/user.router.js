41b8af83ebf61179eb9c05d19876ee3ffbf879cd
VvencesS <DucSoanVences99@gmail.com>
2020-12-14 14:52:27 +0700

    Upload file dung thuoc tinh enctype='multipart/form-data' cua form va package multer de upload file anh

--- routers/user.router.js
+++ routers/user.router.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const multer  = require('multer');
 
 const controller = require('../controllers/user.controller');
 const validate = require('../validate/user.validate');
+
+const upload = multer({ dest: './public/uploads/' });
 
 router.get('/', controller.index);
 
@@ -12,6 +15,10 @@ router.get('/create', controller.create);
 
 router.get('/:id', controller.get);
 
-router.post('/create', validate.postCreate, controller.postCreate);
+router.post('/create', 
+    upload.single('avatar'), 
+    validate.postCreate, 
+    controller.postCreate
+);
 
 module.exports = router;
